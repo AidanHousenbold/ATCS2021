1f664f2feb8d06533f6515fdeb941df4718e02aa
Aidan Housenbold <HousenboldA@gmail.com>
2022-05-10 09:19:02 -0700

    Theoretical cascade implemented to detect hand in image

--- Identifying_X-Ray/cascadeutils.py
+++ Identifying_X-Ray/cascadeutils.py
@@ -1,7 +1,8 @@
 '''
-https://learncodebygaming.com/blog/training-a-cascade-classifier
+This program was used to organize files for training
 '''
 import os
+import pandas as pd
 
 def generate_negative_description_file():
     # open the output file for writing. will overwrite all existing data in there
@@ -10,4 +11,35 @@ def generate_negative_description_file():
         for filename in os.listdir('data/images/train/hand/negative'):
             f.write('data/images/train/hand/negative/' + filename + '\n')
 
+'''
+This function took in all of the images and created txt files with the paths
+to each image so I could then use the command line
+
+to move each photos into the correct location for positive or negative training 
+'''
+def create_part_textFiles():
+    # load the data
+    labels = pd.read_csv('data/train_df.csv')
+
+    # add '-c.png to labels
+    for path in range(0, len(labels['SOPInstanceUID'].values)):
+        labels.at[path, 'SOPInstanceUID'] = labels.at[path, 'SOPInstanceUID'] + '-c.png'
+
+    filePaths = labels['SOPInstanceUID'].values
+    print(filePaths)
+    images = pd.DataFrame()
+    images = labels[['SOPInstanceUID', 'Target']]
+
+    # Add only the one target to each of its respective txt files
+    for image in range(0, len(labels['Target']) - 1):
+        targets = images['Target'][image]
+        targets.rstrip()
+        t_list = targets.split(' ')
+        # if len(t_list) == 2:
+        #      path = "data/txt.files/" + str(t_list[0]) + ".txt"
+        #      with open(path, 'a') as f:
+        #          f.write(images['SOPInstanceUID'][image])
+        #          f.write('\n')
+
+#create_part_textFiles()
 #generate_negative_description_file()
--- Identifying_X-Ray/main.py
+++ Identifying_X-Ray/main.py
@@ -2,54 +2,47 @@
 Aidan Housenbold
 X-ray image recognition
 
-refernce:
+How to train a classifier
+https://learncodebygaming.com/blog/training-a-cascade-classifier
 
-#Help for using tensorflow for my AI
-https://www.tensorflow.org/tutorials/images/classification
-
-Help for converting images from DICOM to PNG
-https://www.kaggle.com/code/ibombonato/x-ray-body-part-classification-fastai-starter
-
+https://www.geeksforgeeks.org/detect-an-object-with-opencv-python/
+The code structure below is from the tutorial above and converted for my hand image detector
 '''
 
+import cv2
 import numpy as np
-import os
-import PIL
-import PIL.Image
-import tensorflow as tf
-import tensorflow_datasets as tfds
-import pandas as pd
+from matplotlib import pyplot as plt
 
-#Load the data and organize it
-labels = pd.read_csv('data/train_df.csv')
-instances = labels[['SOPInstanceUID','Target']]
+#Get the would be trainned classifier
+hand_classifier = cv2.CascadeClassifier('handCascade/hand_cascade.XML')
 
-abdomen = []
-ankle = []
-cervicalSpine = []
-chest = []
-clavicles = []
-elbow = []
-feet = []
-finger = []
-forearm = []
-hand = []
-hip = []
-knee = []
-lowerLeg = []
-lumbarSpine = []
-others = []
-pelvis = []
-shoulder = []
-sinus = []
-skull = []
-thigh = []
-thoracicSpine = []
-wrist = []
+#get the test instance you want to classify
+instance = cv2.imread("1.2.826.0.1.3680043.8.498.21206216606416563283367654584074249600-c.png")
 
-for UID in instances['SOPInstanceUID']:
+#Covert the image to gray scale for the model
+gray_scaled_hand = cv2.cvtColor(instance, cv2.COLOR_BGR2GRAY)
+rbg_scaled_hand = cv2.cvtColor(instance, cv2.COLOR_BGR2RGB)
+#give image to the model to check
+detected = hand_classifier.detectMultiScale(gray_scaled_hand,
+                                   minSize =(20, 20))
 
+# Don't do anything if there's
+# no sign
+detected_items = len(detected)
 
+if detected_items != 0:
 
+    # There may be more than one
+    # sign in the image
+    for (x, y, width, height) in detected:
+        # We draw a green rectangle around
+        # every recognized sign
+        cv2.rectangle(rbg_scaled_hand, (x, y),
+                      (x + height, y + width),
+                      (0, 255, 0), 5)
 
-
+# Creates the environment of
+# the picture and shows it
+plt.subplot(1, 1, 1)
+plt.imshow(rbg_scaled_hand)
+plt.show()
